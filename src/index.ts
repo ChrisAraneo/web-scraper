80553d223320a768129cdc79b62297c7d4d9a9bb
Krzysztof Pająk <chris.araneo@gmail.com>
2025-09-02 22:17:42 +0200

    feat: add scrapping list page and detail pages

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,73 @@
-export function concat(str1: string, str2: string): string {
-  return str1 + str2;
+import { config } from 'dotenv';
+import axios from 'axios';
+import JSSoup from 'jssoup';
+
+config();
+
+if (!process.env.LIST_URL) {
+  console.error('LIST_URL is not defined');
+  process.exit(1);
 }
+
+if (!process.env.DETAILS_URL) {
+  console.error('DETAILS_URL is not defined');
+  process.exit(1);
+}
+
+if (!process.env.REGION) {
+  console.error('REGION is not defined');
+  process.exit(1);
+}
+
+const result: any[] = [];
+const total = 1;
+
+async function main() {
+  for (let i = 0; i < total; i++) {
+    const url: string = `${process.env.LIST_URL}?region=${process.env.REGION}&page=${i}`;
+    const list = await fetchListPage(url);
+    const details = list.map((id) => {
+      return fetchDetailsPage(`${process.env.DETAILS_URL}/${process.env.REGION}/${id}/champions`);
+    });
+
+    Promise.all(details).then((values) => {
+      values.forEach((detail) => {
+        result.push(detail);
+      });
+    });
+
+    console.log(`Scrapped ${i} of ${total}`);
+  }
+}
+
+async function fetchListPage(url: string): Promise<string[]> {
+  let ids: string[] = [];
+
+  try {
+    const response = await axios.get(url);
+    const soup = new JSSoup(response.data);
+    const rows = soup.findAll('tr');
+    ids = rows.map((row) => row.attrs?.id).filter(Boolean);
+  } catch (error) {
+    console.error('Error scraping website:', url);
+  }
+
+  return ids;
+}
+
+async function fetchDetailsPage(url: string): Promise<string[]> {
+  let result: string[] = [];
+
+  try {
+    const response = await axios.get(url);
+    const soup = new JSSoup(response.data);
+    const items = soup.findAll('tr');
+    result = items.filter((item) => item.attrs?.class === 'cursor-pointer').map((item) => item.getText('|')).filter(Boolean);
+  } catch (error) {
+    console.error('Error scraping website:', url);
+  }
+
+  return result;
+}
+
+main();
