32f22903f20d63f32b1271dceb7b3fa76de77285
Krzysztof Pająk <chris.araneo@gmail.com>
2025-09-05 21:45:14 +0200

    feat(fetch-details-page): add retry

--- src/fetch-details-page.ts
+++ src/fetch-details-page.ts
@@ -1,6 +1,9 @@
 import JSSoup from 'jssoup';
 import axios from 'axios';
 import { sleepRandomTime } from './sleep-random-time';
+import { sleep } from './sleep';
+
+const FIVE_MINUTES_MS = 1000 * 60 * 5;
 
 export async function fetchDetailsPage(url: string): Promise<string[]> {
   let result: string[] = [];
@@ -16,7 +19,9 @@ export async function fetchDetailsPage(url: string): Promise<string[]> {
       .map((item) => item.getText('|'))
       .filter(Boolean);
   } catch (error) {
-    console.error('Error scraping website:', url);
+    await sleep(FIVE_MINUTES_MS);
+
+    return fetchDetailsPage(url);
   }
 
   return result;
