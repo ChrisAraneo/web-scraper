72283dbbb84c6757c5d24a5408e080103c5fd2e0
Krzysztof Pająk <chris.araneo@gmail.com>
2025-09-09 22:35:19 +0200

    style(fetch-list-page): format code

--- src/fetch-list-page.ts
+++ src/fetch-list-page.ts
@@ -6,9 +6,12 @@ import { get } from 'lodash';
 const TEN_MINUTES_MS = 1000 * 60 * 10;
 
 export function fetchListPage(url: string): Observable<string[]> {
-  const logError = (error: unknown) => tap(() => logger.error(
-    `Error fetching list page ${url}. Status: ${get(error, 'status', 'unknown')}. Retrying...`,
-  ));
+  const logError = (error: unknown) =>
+    tap(() =>
+      logger.error(
+        `Error fetching list page ${url}. Status: ${get(error, 'status', 'unknown')}. Retrying...`,
+      ),
+    );
   const delayTenMinutes = delay(TEN_MINUTES_MS);
 
   return getWebsite(url).pipe(
@@ -22,5 +25,6 @@ export function fetchListPage(url: string): Observable<string[]> {
       logError(error);
 
       return fetchListPage(url).pipe(delayTenMinutes);
-    }));
+    }),
+  );
 }
