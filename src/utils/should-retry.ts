8fbc25b78ea7d1987d462eedf58d903ce4531cda
Krzysztof Pająk <chris.araneo@gmail.com>
2025-09-15 23:27:10 +0200

    feat(should-retry): handle more edge cases

--- src/utils/should-retry.ts
+++ src/utils/should-retry.ts
@@ -1,3 +1,18 @@
-export function shouldRetry(data: any): boolean {
-  return !data?.status || data.status === 429 || data.status / 100 === 5;
+import { isNaN, get, isInteger, isString, isNumber } from 'lodash';
+
+export function shouldRetry(data?: { status?: number } | unknown): boolean {
+  const status = get(data, 'status');
+
+  if (!isString(status) && !isNumber(status)) {
+    return true;
+  }
+
+  const numberStatus = Number(status);
+
+  return (
+    !isInteger(numberStatus) ||
+    isNaN(numberStatus) ||
+    numberStatus === 429 ||
+    (numberStatus >= 500 && numberStatus <= 599)
+  );
 }
