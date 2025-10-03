8d9bcac08138513ba48c5d773b5dcf9055b1ca8d
Krzysztof Pająk <chris.araneo@gmail.com>
2025-10-03 20:30:23 +0200

    fix(logger): fix getting LOG_LEVEL env variable

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,3 @@
 import { Logger, LogLevel } from '@chris.araneo/logger';
-import { getEnvOrExitProcess } from './get-env-or-exit-process';
 
-const LOG_LEVEL = getEnvOrExitProcess('LOG_LEVEL');
-
-export const logger = new Logger(LOG_LEVEL as LogLevel);
+export const logger = new Logger(process.env['LOG_LEVEL'] as LogLevel || 'info');
