8fedcd8318b83e0d77c8bca5daf7f47c531061bf
Krzysztof Pająk <chris.araneo@gmail.com>
2025-10-03 20:10:21 +0200

    feat(logger): pass LOG_LEVEL variable

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,3 +1,6 @@
-import { Logger } from '@chris.araneo/logger';
+import { Logger, LogLevel } from '@chris.araneo/logger';
+import { getEnvOrExitProcess } from './get-env-or-exit-process';
 
-export const logger = new Logger();
+const LOG_LEVEL = getEnvOrExitProcess('LOG_LEVEL');
+
+export const logger = new Logger(LOG_LEVEL as LogLevel);
