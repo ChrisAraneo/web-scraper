550942f178b4122acf5899057f7d5c2a401ee8d6
Krzysztof Pająk <chris.araneo@gmail.com>
2025-10-04 20:49:25 +0200

    feat(get-website): add debug logs

--- src/get-website.ts
+++ src/get-website.ts
@@ -1,9 +1,21 @@
 import JSSoup from 'jssoup';
 import axios from 'axios';
 import { from, Observable } from 'rxjs';
+import { logger } from './utils/logger';
 
 export function getWebsite(url: string): Observable<any> {
   return from(
-    axios.get(encodeURI(url)).then((response) => new JSSoup(response.data)),
+    axios
+      .get(encodeURI(url))
+      .then((response) => {
+        logger.debug(`Fetched website ${url}`, response?.status);
+
+        return new JSSoup(response.data);
+      })
+      .catch((error) => {
+        logger.debug(`Error fetching website ${url}`, error?.status);
+
+        throw error;
+      }),
   );
 }
